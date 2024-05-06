eb25d542963d2f070b9151381730344ddfec5215
Felipe Bastos <felipe.bastos.dev@gmail.com>
2024-05-06 19:28:54 -0300

    feat: creating header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
+import { Header } from './components/header'
+
 export function App() {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold">Hello World</h1>
-    </div>
-  )
+  return <Header />
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+import { Armchair } from 'lucide-react'
+
+export function Header() {
+  return (
+    <header className="flex items-center justify-center py-4 bg-zinc-900">
+      <Armchair className="h-10 w-10" color="#fafafa" />
+    </header>
+  )
+}
