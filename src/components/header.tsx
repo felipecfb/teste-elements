d74704cec778d61640135efc6dabf48f0e353cd8
Felipe Bastos <felipe.bastos.dev@gmail.com>
2024-05-13 09:01:46 -0300

    feat: adding a link to logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,12 @@
 import { Armchair } from 'lucide-react'
+import { Link } from 'react-router-dom'
 
 export function Header() {
   return (
-    <header className="flex items-center justify-center py-4 bg-zinc-900">
-      <Armchair className="h-10 w-10" color="#9333EA" />
-    </header>
+    <Link to="/">
+      <header className="flex items-center justify-center py-4 bg-zinc-900">
+        <Armchair className="h-10 w-10" color="#9333EA" />
+      </header>
+    </Link>
   )
 }
